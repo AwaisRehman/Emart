548d2efc1e6880886110dd9fae688fa16033d17e
AwaisRehman <92169509+AwaisRehman@users.noreply.github.com>
2022-04-29 10:31:59 +0300

    React Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Products from './components/Products';
+import Product from './components/Product';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Cart from './components/Cart';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+     <Navbar />
+     
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home />} />
+      </Route>
+      <Route path="/products">
+        <Route index element={<Products />} />
+      </Route>
+      <Route path="/products/:id">
+        <Route index element={<Product />} />
+      </Route>
+      <Route path="/cart">
+        <Route index element={<Cart />} />
+      </Route>
+    </Routes>
+    
+    </>
+
   );
 }
 
